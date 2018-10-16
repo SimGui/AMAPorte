ceb3248d82931dc43b3d248e9d5c2f338c501e3e
Simon <simon@webnlog.ovh>
2018-10-16 21:37:45 +0200

    Add search icon in app bar and delete settings entry

--- app/src/main/java/ovh/webnlog/amaporte/ui/NavigationActivity.java
+++ app/src/main/java/ovh/webnlog/amaporte/ui/NavigationActivity.java
@@ -47,11 +47,6 @@ public class NavigationActivity extends AppCompatActivity implements NavigationV
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
         return super.onOptionsItemSelected(item);
     }
 
