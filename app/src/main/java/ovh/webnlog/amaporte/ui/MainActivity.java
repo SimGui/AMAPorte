571d4ac5776e22fb5abf8ad618c217b72c94394d
Simon <simon@webnlog.ovh>
2018-10-16 14:08:44 +0200

    Add custom marker at user location

--- app/src/main/java/ovh/webnlog/amaporte/ui/MainActivity.java
+++ app/src/main/java/ovh/webnlog/amaporte/ui/MainActivity.java
@@ -1,8 +1,6 @@
 package ovh.webnlog.amaporte.ui;
 
-import android.Manifest;
 import android.annotation.SuppressLint;
-import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
 
@@ -10,12 +8,9 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.NavigationView;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.widget.Toast;
 
 import com.mapbox.android.core.location.LocationEngine;
 import com.mapbox.android.core.location.LocationEngineListener;
@@ -24,21 +19,17 @@ import com.mapbox.android.core.location.LocationEngineProvider;
 import com.mapbox.android.core.permissions.PermissionsListener;
 import com.mapbox.android.core.permissions.PermissionsManager;
 import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.annotations.Icon;
+import com.mapbox.mapboxsdk.annotations.IconFactory;
 import com.mapbox.mapboxsdk.annotations.MarkerOptions;
+import com.mapbox.mapboxsdk.annotations.MarkerViewOptions;
 import com.mapbox.mapboxsdk.camera.CameraPosition;
 import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
-import com.mapbox.mapboxsdk.constants.Style;
 import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
-import com.mapbox.mapboxsdk.maps.MapboxMapOptions;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 import com.mapbox.mapboxsdk.plugins.localization.LocalizationPlugin;
-import com.mapbox.mapboxsdk.plugins.localization.MapLocale;
-import com.mapbox.mapboxsdk.plugins.locationlayer.LocationLayerPlugin;
-import com.mapbox.mapboxsdk.plugins.locationlayer.modes.CameraMode;
-import com.mapbox.mapboxsdk.plugins.locationlayer.modes.RenderMode;
-import com.mapbox.mapboxsdk.style.light.Position;
 
 import java.util.List;
 
@@ -60,67 +51,18 @@ public class MainActivity extends NavigationActivity implements OnMapReadyCallba
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if(permissionDenied()) {
-            permissionsManager = new PermissionsManager(this);
-            permissionsManager.requestLocationPermissions(this);
-        }
-
+        initPermissionManager();
         initMapBox(savedInstanceState);
         initNavigation();
     }
+
 
     @Override
     public void onMapReady(MapboxMap mapboxMap) {
         map = mapboxMap;
 
         setMapBoxLanguage(mapboxMap);
-
-        if(!permissionDenied()) {
-            setLocationEngine();
-        }
-    }
-
-    @SuppressLint("MissingPermission")
-    private void setLocationEngine() {
-        locationEngineProvider = new LocationEngineProvider(this);
-        locationEngine = locationEngineProvider.obtainBestLocationEngineAvailable();
-        locationEngine.addLocationEngineListener(this);
-        locationEngine.setPriority(LocationEnginePriority.HIGH_ACCURACY);
-        locationEngine.activate();
-
-    }
-
-    // PRIVATE METHODS
-
-    private void initMapBox(Bundle savedInstanceState) {
-        Mapbox.getInstance(this, Constant.MAPBOX_TOKEN);
-
-        mapView = findViewById(R.id.mapView);
-        mapView.onCreate(savedInstanceState);
-        mapView.getMapAsync(this);
-    }
-
-    private void initNavigation() {
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show());
-
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
-
-        NavigationView navigationView = findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-    }
-
-    private void setMapBoxLanguage(MapboxMap mapboxMap) {
-        localizationPlugin = new LocalizationPlugin(mapView, mapboxMap);
-        localizationPlugin.matchMapLanguageWithDeviceDefault();
+        setLocationEngine();
     }
 
     @Override
@@ -205,20 +147,76 @@ public class MainActivity extends NavigationActivity implements OnMapReadyCallba
         moveCamera(location);
     }
 
+    //region PRIVATE METHODS
+
+    private void initMapBox(Bundle savedInstanceState) {
+        Mapbox.getInstance(this, Constant.MAPBOX_TOKEN);
+
+        mapView = findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(this);
+    }
+
+    private void initNavigation() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show());
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @SuppressLint("MissingPermission")
+    private void setLocationEngine() {
+        if(!permissionDenied()) {
+            locationEngineProvider = new LocationEngineProvider(this);
+            locationEngine = locationEngineProvider.obtainBestLocationEngineAvailable();
+            locationEngine.addLocationEngineListener(this);
+            locationEngine.setPriority(LocationEnginePriority.HIGH_ACCURACY);
+            locationEngine.activate();
+        }
+    }
+
+    private void setMapBoxLanguage(MapboxMap mapboxMap) {
+        localizationPlugin = new LocalizationPlugin(mapView, mapboxMap);
+        localizationPlugin.matchMapLanguageWithDeviceDefault();
+    }
+
     private void moveCamera(Location location) {
         CameraPosition position = new CameraPosition.Builder()
-                .target(new LatLng(location.getLatitude(), location.getLongitude())) // Sets the new camera position
-                .zoom(12) // Sets the zoom
-                .build(); // Creates a CameraPosition from the builder
+                .target(new LatLng(location.getLatitude(), location.getLongitude()))
+                .zoom(12)
+                .build();
 
         map.animateCamera(CameraUpdateFactory
                 .newCameraPosition(position), 5000);
 
+        IconFactory iconFactory = IconFactory.getInstance(MainActivity.this);
+        Icon icon = iconFactory.fromResource(R.drawable.ic_location_blue_dot);
         map.addMarker(new MarkerOptions()
-                .position(new LatLng(location.getLatitude(), location.getLongitude())));
+                .position(new LatLng(location.getLatitude(), location.getLongitude()))
+                .icon(icon));
+    }
+
+    private void initPermissionManager() {
+        if(permissionDenied()) {
+            permissionsManager = new PermissionsManager(this);
+            permissionsManager.requestLocationPermissions(this);
+        }
     }
 
     private boolean permissionDenied() {
         return !PermissionsManager.areLocationPermissionsGranted(this);
     }
+
+    //endRegion
 }
