2e33ba3a6f9efd8402e52de2f651dc04832fbfff
Simon <simon@webnlog.ovh>
2018-10-19 14:21:23 +0200

    Add MapsManager for manage localisation

---
+++ app/src/main/java/ovh/webnlog/amaporte/utils/MapsManager.java
@@ -0,0 +1,147 @@
+package ovh.webnlog.amaporte.utils;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+
+import com.mapbox.android.core.permissions.PermissionsListener;
+import com.mapbox.android.core.permissions.PermissionsManager;
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.camera.CameraPosition;
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
+import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.location.LocationComponent;
+import com.mapbox.mapboxsdk.location.modes.CameraMode;
+import com.mapbox.mapboxsdk.location.modes.RenderMode;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mapbox.mapboxsdk.plugins.localization.LocalizationPlugin;
+
+import java.util.List;
+
+import ovh.webnlog.amaporte.R;
+import ovh.webnlog.amaporte.ui.MainActivity;
+
+import static android.content.Context.LOCATION_SERVICE;
+
+public class MapsManager implements OnMapReadyCallback, PermissionsListener {
+
+    public MapView mapView;
+    private MapboxMap map;
+    private LocalizationPlugin localizationPlugin;
+    private LocationComponent locationComponent;
+    private PermissionsManager permissionsManager;
+    private Context context;
+
+    public MapsManager(MapView mapView, Context context) {
+        this.mapView = mapView;
+        this.context = context;
+    }
+
+    public void initMapBox(Bundle savedInstanceState) {
+        if(gpsTurnOff()) {
+            showGPSDisabledAlertToUser();
+        }
+
+        Mapbox.getInstance(context, Constant.MAPBOX_TOKEN);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(this);
+    }
+
+    @SuppressLint("MissingPermission")
+    @Override
+    public void onMapReady(MapboxMap mapboxMap) {
+        map = mapboxMap;
+
+        setMapBoxLanguage(mapboxMap);
+        setLocationComponent();
+    }
+
+    private void setMapBoxLanguage(MapboxMap mapboxMap) {
+        localizationPlugin = new LocalizationPlugin(mapView, mapboxMap);
+        localizationPlugin.matchMapLanguageWithDeviceDefault();
+    }
+
+    @SuppressLint("MissingPermission")
+    public void setLocationComponent() {
+        if(permissionDenied()) {
+            initPermissionManager();
+            return;
+        }
+
+        locationComponent = map.getLocationComponent();
+        locationComponent.activateLocationComponent(context);
+        locationComponent.setLocationComponentEnabled(true);
+        locationComponent.setRenderMode(RenderMode.NORMAL);
+        locationComponent.setCameraMode(CameraMode.TRACKING);
+        locationComponent.zoomWhileTracking(12,2000);
+    }
+
+    private boolean gpsTurnOff() {
+        LocationManager locationManager = (LocationManager) context.getSystemService(LOCATION_SERVICE);
+
+        return !locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+    }
+
+    @SuppressLint("MissingPermission")
+    public void moveCamera() {
+
+        if(permissionDenied()) {
+            initPermissionManager();
+            return;
+        }
+
+        Location location = locationComponent.getLastKnownLocation();
+
+        if(location != null) {
+            CameraPosition position = new CameraPosition.Builder()
+                    .target(new LatLng(location.getLatitude(), location.getLongitude()))
+                    .zoom(12)
+                    .build();
+
+            map.easeCamera(CameraUpdateFactory.newCameraPosition(position), 2000);
+        }
+    }
+
+    private void showGPSDisabledAlertToUser(){
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
+        alertDialogBuilder.setMessage(R.string.ask_gps_activation)
+                .setCancelable(false)
+                .setPositiveButton("OK",
+                        (dialog, id) -> {
+                            dialog.cancel();
+                        });
+        AlertDialog alert = alertDialogBuilder.create();
+        alert.show();
+    }
+
+    private void initPermissionManager() {
+        if(permissionDenied()) {
+            permissionsManager = new PermissionsManager(this);
+            permissionsManager.requestLocationPermissions((Activity) context);
+        }
+    }
+
+    private boolean permissionDenied() {
+        return !PermissionsManager.areLocationPermissionsGranted(context);
+    }
+
+    @Override
+    public void onExplanationNeeded(List<String> permissionsToExplain) {
+
+    }
+
+    @Override
+    public void onPermissionResult(boolean granted) {
+        if(granted) {
+            setLocationComponent();
+        }
+    }
+}
